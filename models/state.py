37ce5026a04fcf77f86ffd6346bbfd8328665e4a
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-04-20 21:07:28 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
+"""Defines the State class."""
 import models
 import os
-from models.city import City
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String, ForeignKey
 from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """Represents a state """
-    if models.storage_t == 'db':
-        __tablename__ = "states"
-        name = Column(String(128), nullable=False)
-        cities = relationship("City",  backref="state", cascade="delete")
-    else:
+    """Represents a state for a MySQL database.
+    Inherits from SQLAlchemy Base and links to the MySQL table states.
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store States.
+        name (sqlalchemy String): The name of the State.
+        cities (sqlalchemy relationship): The State-City relationship.
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
+
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
             """Get a list of all related City objects."""
