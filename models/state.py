cad1b3984c15738e5a93ff460d5417e26e6c3da7
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-04-20 16:28:21 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -22,7 +22,7 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    if os.getenv('HBNB_TYPE_STORAGE') != "db":
+    if models.storage_type == 'db':
         cities = relationship("City",  backref="state", cascade="delete")
     else:
         @property
