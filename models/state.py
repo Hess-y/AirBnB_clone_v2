c2b016429f910ea53b6538bf57eff60761c040cd
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-04-20 16:33:12 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
+from models.base_model import Base
 import models
 import os
 from models.city import City
