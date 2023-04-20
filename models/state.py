9c29220c0247f86bf3a96b24feca0f7c3435b6bc
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-04-20 15:28:06 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -20,9 +20,9 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    if models.storage_type == 'db':
+    cities = relationship("City",  backref="state", cascade="delete")
         @property
         def cities(self):
             """Get a list of all related City objects."""
@@ -31,3 +31,12 @@ class State(BaseModel, Base):
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
+
+def __init__(self, *args, **kwargs):
+        """Initialize a new State.
+        
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        super().__init__(*args, **kwargs)
