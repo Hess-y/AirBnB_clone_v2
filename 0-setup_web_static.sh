95eb43a89cd966068afe89b9ac27a050989c040d
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-05-08 21:15:59 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
 #Write a Bash script that sets up your web servers for the deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+conf="\\\n\tlocation /hbnb_static/ {\n\t\alias /data/web_static/current/;\n\t}"
+sudo sed -i "45i $conf" /etc/nginx/sites-available/default
+sudo service nginx start
+sudo service nginx restart
+sudo service nginx reload
