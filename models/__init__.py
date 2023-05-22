6345f13a9819fe18f6919d7d0feaa95b238a60d0
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-05-22 15:16:50 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,7 @@ If the environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
    instantiates a database storage engine (DBStorage).
 Otherwise, instantiates a file storage engine (FileStorage).
 """
-import os
+from os import getenv
 
 
 if os.getenv("HBNB_TYPE_STORAGE") == "db":
