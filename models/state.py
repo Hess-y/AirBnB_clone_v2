4bd5939a95ba98b4fdc66482a05e92cbbbbc39d0
Hess-y <113606328+Hess-y@users.noreply.github.com>
2023-05-22 14:35:13 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -3,6 +3,8 @@
 import models
 import os
 from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
 from models.base_model import BaseModel
 from models.city import City
